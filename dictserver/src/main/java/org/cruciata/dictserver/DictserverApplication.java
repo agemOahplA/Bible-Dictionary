a5e30ab9032d5b607e84073fb1c2737f2eead789
mht <moahotainlll@163.com>
2021-09-16 16:24:42 +0800

    基础

---
+++ dictserver/src/main/java/org/cruciata/dictserver/DictserverApplication.java
@@ -0,0 +1,13 @@
+package org.cruciata.dictserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DictserverApplication.class, args);
+    }
+
+}
---
+++ dictserver/src/test/java/org/cruciata/dictserver/DictserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cruciata.dictserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DictserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
