cb67855ad95d1264dd1904ad7f36fb5080efb3d1
agemOahplA <spawnmht7nothing@gmail.com>
2021-11-22 17:02:38 +0800

    修改域名

--- dictserver/build.sh
+++ dictserver/build.sh
@@ -5,7 +5,7 @@ mvn clean package
 # 编译镜像
 echo "Docker编译镜像"
 # shellcheck disable=SC2046
-docker build . --tag  dict.cruciata.org/dictserver:1.0.0
+docker build . --tag  dict.vivevia.info/dictserver:1.0.0
 
 echo "镜像列表"
 
